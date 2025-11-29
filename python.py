bc65de2ba34a0ef91fe8e9c18d88863de29c5526
Ahmadiqbal4081 <aajgatp@gmail.com>
2025-11-29 16:19:09 +0500

    Initial commit

---
+++ python.py
@@ -0,0 +1,24 @@
+# percentage calculator
+
+obt_marks =50
+total_marks =100
+
+print(total_marks)
+print(obt_marks)
+
+obt_percentage =((obt_marks)/(total_marks)*100)
+print (type(obt_percentage))
+print("Obtained Percentage:", obt_percentage, "%")
+
+# end of code
+
+print("hello")
+print("i am working")
+print("this is phython code")
+
+name = "Ahmad"
+condition = False
+
+print(name)
+print(condition)
+
---
+++ speak.vbs
@@ -0,0 +1,2 @@
+Set Speak=CreateObject("sapi.spvoice")
+Speak.Speak "hey how are you.I am text to voice converter"
